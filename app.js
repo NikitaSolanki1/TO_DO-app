3fc24f413bee94d81516008dea502226d4a9a5d0
NikitaSolanki1 <147810621+NikitaSolanki1@users.noreply.github.com>
2024-08-27 22:34:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,138 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+
+    if (storedTasks) {
+        storedTasks.forEach((task) => tasks.push(task));
+        updateTaskList();
+        updateStats();
+    }
+});
+
+let tasks = [];
+
+const saveTasks = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const addTask = () => {
+    const taskInput = document.getElementById('taskInput');
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text: text, completed: false });
+        taskInput.value = "";
+        updateTaskList();
+        saveTasks();
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTaskList();
+    saveTasks();
+
+    // Trigger confetti when all tasks are completed
+    if (tasks.every(task => task.completed)) {
+        blastConfetti();
+    }
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTaskList();
+    saveTasks();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById('taskInput');
+    taskInput.value = tasks[index].text;
+
+    tasks.splice(index, 1);
+    updateTaskList();
+    saveTasks();
+};
+
+const updateStats = () => {
+    const completedTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = (completedTasks / totalTasks) * 100;
+    const progressBar = document.getElementById('progress');
+
+    progressBar.style.width = `${progress}%`;
+
+    document.getElementById('numbers').innerText = `${completedTasks} / ${totalTasks}`;
+};
+
+const updateTaskList = () => {
+    const taskList = document.querySelector('.task-list');
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement('li');
+
+        listItem.innerHTML = `
+        <div class="taskItem">
+            <div class="task ${task.completed ? "completed" : ""}">
+                <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+                <p>${task.text}</p>
+            </div>
+            <div class="icons">
+                <img src="./img/pen.png" onClick="editTask(${index})"/>
+                <img src="./img/trash.png" onClick="deleteTask(${index})"/>
+            </div>    
+        </div>
+        `;
+
+        listItem.querySelector('.checkbox').addEventListener('change', () => toggleTaskComplete(index));
+        taskList.appendChild(listItem);
+    });
+
+    updateStats();
+};
+
+document.getElementById('newTask').addEventListener('click', function(e) {
+    e.preventDefault();
+    addTask();
+});
+
+const blastConfetti = () => {
+    const count = 200;
+    const defaults = {
+        origin: { y: 0.7 },
+    };
+
+    function fire(particleRatio, opts) {
+        confetti(
+            Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio),
+            })
+        );
+    }
+
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+
+    fire(0.2, {
+        spread: 60,
+    });
+
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+};
